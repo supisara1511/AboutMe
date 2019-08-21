0eb2541c0abaaaf430e18464bf63f676d77349f8
Supisara Prasittinawa <supisara_2540@hotmail.com>
2019-08-22 01:49:59 +0700

    first commit

--- app/src/main/java/buu/informatics/s59160134/aboutme/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160134/aboutme/MainActivity.kt
@@ -15,6 +15,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val myName: MyName = MyName("Supisara Prasittinawa")
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.apply {
